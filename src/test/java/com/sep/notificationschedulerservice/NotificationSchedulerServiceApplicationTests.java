44aab1abd802f8e851e1d61ded2f4befa206e972
Charith Banneheka <charithbanneheka123@gmail.com>
2024-03-28 02:13:15 +0530

    Initial Commit

---
+++ src/test/java/com/sep/notificationschedulerservice/NotificationSchedulerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sep.notificationschedulerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationSchedulerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
